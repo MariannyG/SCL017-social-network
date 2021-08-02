7207e6b1e3915dee6f4baca069ce58cc5f892ac1
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-08-02 11:17:12 -0400

    Nueva rama para pantalla home

--- src/lib/views/Home.js
+++
@@ -1,9 +0,0 @@
-
-export default ()=>{
-    const homeView=`<div>
-    <a href="#profile">profile</a>
-    </div>`;
-    const homePage=document.createElement('section');
-    homePage.innerHTML=homeView;
-    return homePage;
-}
