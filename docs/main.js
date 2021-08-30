dcb47f9aa5867e16ded11417c3f274730b408d82
Marianny Garcia <mdvgarciar@gmail.com>
2021-08-30 09:41:09 -0400

    files

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
-import { changeRoute } from "../lib/controllers/route.js";
-import { hrefContent, routes } from "../lib/views/Components.js";
+import { changeRoute } from "./lib/controllers/route.js";
+import { hrefContent, routes } from "./lib/views/Components.js";
 
 
 const container = document.getElementById("root");
